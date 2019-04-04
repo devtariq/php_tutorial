0954c94f5cc39b06cf2fba1d3081d7b6429b8761
Tariuql Islam <menone@gmail.com>
2019-04-04 12:45:48 +0600

    add new file variable variable

---
+++ top.php
@@ -0,0 +1,360 @@
+<div style="box-shadow: 0 0px 30px #cccccc;">
+    <div style="background: #009CDE;padding: 10px 0px;">
+        <h4 style="color: white; text-align: center;  margin: 0px; padding: 5px 0px; font-size: 25px;font-family:'GothamBold';">Sat, June 2nd 2019</h4>
+    </div>
+
+    <div class="schedule_container">
+        <h4 style="color: #1b1b1b; font-weight: bold; font-size: 25px;">CME Conference Day 1 - The Beverly Hilton Hotel</h4>
+        <p style="font-size: 18px; color: #717171;">Attend lectures given by world-renowned physicians from 15 countries and earn up to 8 CME Credits.
+
+        </p>
+        <!-- Feroz's code start here -->
+
+        <div class="schedule_list_table">
+            <div class="schedule_time_tab">
+                <div class="schedule_time">
+                    <i class="fa fa-clock-o"></i>
+                    <span>6:45am - 7:45am</span>
+                </div>
+                <div class="schedule_name">
+                    <h4>
+                        Breakfast, Wilshire Garden
+                    </h4>
+                </div>
+            </div>
+        </div>
+        <div class="schedule_list_table">
+            <div class="schedule_time_tab">
+                <div class="schedule_time">
+                    <i class="fa fa-clock-o"></i>
+                    <span>7:45am - 8:00am</span>
+                </div>
+                <div class="schedule_name">
+                    <h4>
+                        Welcome
+                    </h4>
+                </div>
+            </div>
+        </div>
+        <div class="schedule_list_table">
+            <div class="schedule_time_tab">
+                <div class="schedule_time">
+                    <i class="fa fa-clock-o"></i>
+                    <span>8:00am - 10:00am</span>
+                </div>
+                <div class="schedule_name">
+                    <h4>
+                        CME Session 1
+                    </h4>
+                    <i class="fa fa-angle-right"></i>
+                </div>
+            </div>
+
+            <div class="schedule_details">
+                <div class="schedule_topic topic_even">
+                    <h5><i class="fa fa-edit"></i> Topic: Who Owns My Stem Cells: A Legal and Regularity Update</h5>
+                    <div class="detail_info">
+                        <ul>
+                            <li>category: <span>Cell Science</span></li>
+                            <li>Location: <span>Beverly Hills Ballroom</span></li>
+                        </ul>
+                        <ul>
+                            <li>CME Accredited: <span>Yes</span></li>
+                            <li>Presented By: <span>Dr. Kristin Comella PhD </span></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="schedule_topic topic_odd">
+                    <h5><i class="fa fa-edit"></i> Topic: A Simple Algorithmic Approach to the Treatment of Spine Pain
+                        Using Stem Cells
+                    </h5>
+                    <div class="detail_info">
+                        <ul>
+                            <li>category: <span>Cell Science</span></li>
+                            <li>Location: <span>Beverly Hills Ballroom</span></li>
+                        </ul>
+                        <ul>
+                            <li>CME Accredited: <span>Yes</span></li>
+                            <li>Presented By: <span>Dr. Harry Adelson MD</span></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="schedule_topic topic_odd">
+                    <h5><i class="fa fa-edit"></i> Topic: Knees, Hips, and Shoulders</h5>
+                    <div class="detail_info">
+                        <ul>
+                            <li>category: <span>Orthopedic Biologics</span></li>
+                            <li>Location: <span>Beverly Hills Ballroom</span></li>
+                        </ul>
+                        <ul>
+                            <li>CME Accredited: <span>Yes</span></li>
+                            <li>Presented By: <span>Dr. Tom Grogan MD</span></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="schedule_topic topic_even">
+                    <h5><i class="fa fa-edit"></i> Topic: Precision Cell Deployment with Radiology</h5>
+                    <div class="detail_info">
+                        <ul>
+                            <li>category: <span>Radiology</span></li>
+                            <li>Location: <span>Beverly Hills Ballroom</span></li>
+                        </ul>
+                        <ul>
+                            <li>CME Accredited: <span>Yes</span></li>
+                            <li>Presented By: <span>Dr. John Feller MD</span></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="schedule_list_table">
+            <div class="schedule_time_tab">
+                <div class="schedule_time">
+                    <i class="fa fa-clock-o"></i>
+                    <span>10:00am - 10:30am</span>
+                </div>
+                <div class="schedule_name">
+                    <h4>
+                        Coffee Break with Exhibitors -
+                        Beverly Hills Ballroom Foyer/Salon
+                    </h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="schedule_list_table">
+            <div class="schedule_time_tab">
+                <div class="schedule_time">
+                    <i class="fa fa-clock-o"></i>
+                    <span>10:30am - 12:00pm</span>
+                </div>
+                <div class="schedule_name">
+                    <h4>
+                        CME Session 2
+                    </h4>
+                    <i class="fa fa-angle-right"></i>
+                </div>
+            </div>
+            <div class="schedule_details">
+                <div class="schedule_topic topic_odd">
+                    <h5><i class="fa fa-edit"></i> Topic: Nitric Oxide Functionality in Regeneration</h5>
+                    <div class="detail_info">
+                        <ul>
+                            <li>category: <span>Cell Signaling</span></li>
+                            <li>Location: <span>Beverly Hills Ballroom</span></li>
+                        </ul>
+                        <ul>
+                            <li>CME Accredited: <span>Yes</span></li>
+                            <li>Presented By: <span>Dr. Nathan Bryan PhD </span></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="schedule_topic topic_even">
+                    <h5><i class="fa fa-edit"></i>Topic: ADSCs in Plastics & Therapeutics</h5>
+                    <div class="detail_info">
+                        <ul>
+                            <li>category: <span>Stem Cells/Plastics</span></li>
+                            <li>Location: <span>Beverly Hills Ballroom</span></li>
+                        </ul>
+                        <ul>
+                            <li>CME Accredited: <span>Yes</span></li>
+                            <li>Presented By: <span>Dr. Kotaro Yoshimura MD</span></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="schedule_topic topic_odd">
+                    <h5><i class="fa fa-edit"></i>Topic: - Specifics of Exosomes and Growth Factors
+                    </h5>
+                    <div class="detail_info">
+                        <ul>
+                            <li>category: <span>Growth Factors/Exosomes</span></li>
+                            <li>Location: <span>Beverly Hills Ballroom</span></li>
+                        </ul>
+                        <ul>
+                            <li>CME Accredited: <span>Yes</span></li>
+                            <li>Presented By: <span>Dr. Ahmed Al Qahtani MD, PhD</span></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="schedule_list_table">
+            <div class="schedule_time_tab">
+                <div class="schedule_time">
+                    <i class="fa fa-clock-o"></i>
+                    <span>12:00pm - 1:25pm</span>
+                </div>
+                <div class="schedule_name">
+                    <h4>
+                        Lunch
+                        Wilshire Garden
+                    </h4>
+                </div>
+            </div>
+        </div>
+        <div class="schedule_list_table">
+            <div class="schedule_time_tab">
+                <div class="schedule_time">
+                    <i class="fa fa-clock-o"></i>
+                    <span>1:25pm - 2:40pm</span>
+                </div>
+                <div class="schedule_name">
+                    <h4>
+                        CME Session 3
+                    </h4>
+                    <i class="fa fa-angle-right"></i>
+                </div>
+            </div>
+            <div class="schedule_details">
+                <div class="schedule_topic topic_odd">
+                    <h5><i class="fa fa-edit"></i> Topic: The Future of SVF and Cell Culture</h5>
+                    <div class="detail_info">
+                        <ul>
+                            <li>category: <span>Stem Cells/Cell Culture</span></li>
+                            <li>Location: <span>Beverly Hills Ballroom</span></li>
+                        </ul>
+                        <ul>
+                            <li>CME Accredited: <span>Yes</span></li>
+                            <li>Presented By: <span>Dr. Hee Young Lee MD</span></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="schedule_topic topic_even">
+                    <h5><i class="fa fa-edit"></i>Topic: - Clinical Decision Making in Regenerative Medicine</h5>
+                    <div class="detail_info">
+                        <ul>
+                            <li>category: <span>Cells for Spine</span></li>
+                            <li>Location: <span>Beverly Hills Ballroom</span></li>
+                        </ul>
+                        <ul>
+                            <li>CME Accredited: <span>Yes</span></li>
+                            <li>Presented By: <span>Dr. Harry Adelson ND</span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="schedule_list_table">
+            <div class="schedule_time_tab">
+                <div class="schedule_time">
+                    <i class="fa fa-clock-o"></i>
+                    <span>2:40pm - 3:00pm</span>
+                </div>
+                <div class="schedule_name">
+                    <h4>
+                        Coffee Break with Exhibitors -
+                        Beverly Hills Ballroom Foyer/Salon
+                    </h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="schedule_list_table">
+            <div class="schedule_time_tab">
+                <div class="schedule_time">
+                    <i class="fa fa-clock-o"></i>
+                    <span>3:00pm - 4:00pm</span>
+                </div>
+                <div class="schedule_name">
+                    <h4>
+                        CME Session 4
+                    </h4>
+                    <i class="fa fa-angle-right"></i>
+                </div>
+            </div>
+            <div class="schedule_details">
+                <div class="schedule_topic topic_odd">
+                    <h5><i class="fa fa-edit"></i> Topic: - Mitochondrial Transfer in Cell Therapy in Neuro</h5>
+                    <div class="detail_info">
+                        <ul>
+                            <li>category: <span>Neuro</span></li>
+                            <li>Location: <span>Beverly Hills Ballroom</span></li>
+                        </ul>
+                        <ul>
+                            <li>CME Accredited: <span>Yes</span></li>
+                            <li>Presented By: <span>Dr. Ilene Ruhoy MD, PhD</span></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="schedule_topic topic_even">
+                    <h5><i class="fa fa-edit"></i> Topic: - Regenerative Ophthalmology
+                    </h5>
+                    <div class="detail_info">
+                        <ul>
+                            <li>category: <span>Ophthalmology Cellular</span></li>
+                            <li>Location: <span>Beverly Hills Ballroom</span></li>
+                        </ul>
+                        <ul>
+                            <li>CME Accredited: <span>Yes</span></li>
+                            <li>Presented By: <span>Dr. Jeffrey Weiss MD </span></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="schedule_topic topic_odd">
+                    <h5><i class="fa fa-edit"></i> Topic: Neurological Advancements from Lab to Clinic
+                    </h5>
+                    <div class="detail_info">
+                        <ul>
+                            <li>category: <span>Neuro</span></li>
+                            <li>Location: <span>Beverly Hills Ballroom</span></li>
+                        </ul>
+                        <ul>
+                            <li>CME Accredited: <span>Yes</span></li>
+                            <li>Presented By: <span>Dr. Oleg Kopyov MD, PhD</span></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="schedule_list_table">
+            <div class="schedule_time_tab">
+                <div class="schedule_time">
+                    <i class="fa fa-clock-o"></i>
+                    <span>4:00pm - 4:30pm </span>
+                </div>
+                <div class="schedule_name">
+                    <h4>
+                        Coffee Break with Exhibitors -
+                        Beverly Hills Ballroom Foyer/Salon
+                    </h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="schedule_list_table">
+            <div class="schedule_time_tab">
+                <div class="schedule_time">
+                    <i class="fa fa-clock-o"></i>
+                    <span>4:00pm-6:00pm
+</span>
+                </div>
+                <div class="schedule_name">
+                    <h4>
+                        Image Guidance
+                        Joint Injection Training, (Limit First 40 Physicians Registered to RSVP)
+                    </h4>
+                </div>
+            </div>
+            <div style="padding: 10px 0px 0px 0px;">
+                <h4 style="color: #1b1b1b; font-weight: bold; font-size: 25px;">Charity Dinner Gala - The Beverly Hilton Hotel</h4>
+
+                <hr/>
+                <div class="thurs">
+                    <p><i class="fa fa-clock-o" style="color: #767676;"></i> <span style="color: #009CDE;
+    font-weight: bold; font-size: 14px;">7:00 PM - 12:00 AM</span></p>
+                </div>
+
+                <div style="display:  flex;"><span style="color: #1b1b1b; font-weight: bold;
+    padding-left: 50px; font-size: 20px;">Gala Dinner in Support of the International Cell Surgical Society</span>
+                </div>
+            </div>
+        </div>
