cb85bae5b759ba52942f085ac0833f425af6050a
Tariuql Islam <menone@gmail.com>
2019-04-04 13:25:57 +0600

    price update

--- variables-variable.php
+++ variables-variable.php
@@ -6,7 +6,17 @@ $$city = "13094 square mile ";
 
 echo "$city is the capital of bangladesh <br>";
 
-echo "The size of  $city is  ${$city} "
+echo "The size of  $city is  ${$city} <br>";
+
+$price_for_friday = "Our Friday price 20% off";
+$price_for_saturday = "Our Friday price 23%";
+$price_for_sunday = "Sunday 30% off";
+$today = "saturday";
+
+$price_for_today = ${'price_for_'.$today};
+
+echo $price_for_today;
+
 
 
 ?>
